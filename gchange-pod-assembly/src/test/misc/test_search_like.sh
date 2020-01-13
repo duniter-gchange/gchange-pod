cf45064498473214213543065134f4b5e73de37e
Benoit Lavenier <benoit.lavenier@e-is.pro>
2020-01-13 20:02:20 +0100

    [enh] Upgrade to Cesium+ Pod v1.5.7 (fix like user event codes, and format)

--- gchange-pod-assembly/src/test/misc/test_search_like.sh
+++ gchange-pod-assembly/src/test/misc/test_search_like.sh
@@ -2,13 +2,17 @@ curl -XGET 'http://localhost:9200/like/record/_search?pretty' -d '{
   "query": {
     "bool": {
       "filter": [
-        {"term" : {"id" : "AW8Q5RFa6_tlAKMUKabg"}}
+        {"term" : {"id" : "HTYsdtCzjtdc2hUdN985Apih2Y5tnvPxkaRE7fRYc5Eh"}}
       ],
       "should":
-        {"term" : {"issuer" : "BxGxCA8UrM6HURr8kTiq7gQVwsZtXekG7dmboUGFNG1L"}}
+        {"term" : {"issuer" : "G2CBgZBPLe6FSFUgpx2Jf1Aqsgta6iib3vmDRA1yLiqU"}}
 
     }
   },
   "size": 1,
-  "_source": [ "issuer" ]
+  "aggs": {
+    "level_sum": {
+      "sum" : { "field" : "level" }
+    }
+  }
 }'
